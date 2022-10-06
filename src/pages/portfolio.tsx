c78c4b0f78e1191fd6ece5c3fb891a87feb68298
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-10-06 17:05:23 +0200

    -

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -1,69 +1,115 @@
 import React, { useState } from 'react';
-import { ModalMask } from '../styles/SC_modal';
+import {
+  ModalMask,
+  ModalPhotoWrapper,
+  ModalProjectDescriptionWrapper
+} from '../styles/SC_modal';
 import {
   PortfolioMainWrapper,
   Photo,
   PhotoContainer,
   FlexRow1,
   FlexRow2,
-  MobileFlexDisplay
+  MobileFlexDisplay,
+  FlexboxWrapper
 } from '../styles/SC_portfolio';
 
 import data from '../../db.json';
+import { config, useSpring } from '@react-spring/web';
 
 const Portfolio: React.FC = (): JSX.Element => {
-  const [modalIsActive, setModalIsActive] = useState(false);
-  const [modalImgSrc, setModalImgSrc] = useState('');
+  const [modalIsActive, setModalIsActive] = useState<boolean>(false);
+  const [modalImgSrc, setModalImgSrc] = useState<string[]>([]);
+  const [modalProjectName, setModalProjectName] = useState<string>('');
   const [desktopModalHeightProperty, setdesktopModalHeightProperty] =
-    useState('');
+    useState<string>('');
 
   const dataRow1 = data.picturesDesktop.row1;
   const dataRow2 = data.picturesDesktop.row2;
   const datatMobile = data.picturesMobile;
 
-  const handleModalIsActive = (url: string, heightProperty: string) => {
+  const handleModalIsActive = (
+    photos: string[],
+    heightProperty: string,
+    projectName: string
+  ) => {
     setModalIsActive((state) => !state);
-    setModalImgSrc(url);
+    setModalImgSrc(photos);
+    setModalProjectName(projectName);
     setdesktopModalHeightProperty(heightProperty);
   };
 
+  const portfolioPageAnimation = useSpring({
+    from: { opacity: 0 },
+    to: { opacity: 1 },
+    config: config.slow
+  });
+
   return (
-    <PortfolioMainWrapper>
+    <PortfolioMainWrapper style={portfolioPageAnimation}>
       {modalIsActive ? (
         <ModalMask onClick={() => setModalIsActive((state) => !state)}>
-          <Photo src={`${modalImgSrc}`} height={desktopModalHeightProperty} />
+          <ModalProjectDescriptionWrapper>
+            <div>
+              <h2>{modalProjectName}</h2>
+              Marta Canert Design
+            </div>
+            <ModalPhotoWrapper>
+              {modalImgSrc.map((url, key) => (
+                <Photo
+                  src={`${url}`}
+                  height={desktopModalHeightProperty}
+                  key={key}
+                />
+              ))}
+            </ModalPhotoWrapper>
+          </ModalProjectDescriptionWrapper>
         </ModalMask>
       ) : null}
-      <FlexRow1>
-        {dataRow1.map((url, key) => (
-          <PhotoContainer key={key}>
-            <Photo
-              src={`${url}`}
-              onClick={() => handleModalIsActive(url, '80vh')}
-            />
-          </PhotoContainer>
-        ))}
-      </FlexRow1>
-      <FlexRow2>
-        {dataRow2.map((url, key) => (
-          <PhotoContainer key={key}>
-            <Photo
-              src={`${url}`}
-              onClick={() => handleModalIsActive(url, '80vh')}
-            />
-          </PhotoContainer>
-        ))}
-      </FlexRow2>
-      <MobileFlexDisplay>
+      <FlexboxWrapper>
+        <FlexRow1>
+          {dataRow1.map((project, key) => (
+            <PhotoContainer key={key}>
+              <Photo
+                src={`${project.photos[0]}`}
+                onClick={() =>
+                  handleModalIsActive(
+                    project.photos,
+                    project.view_height,
+                    project.project_name
+                  )
+                }
+              />
+            </PhotoContainer>
+          ))}
+        </FlexRow1>
+        <FlexRow2>
+          {dataRow2.map((project, key) => (
+            <PhotoContainer key={key}>
+              <Photo
+                src={`${project.photos[0]}`}
+                onClick={() =>
+                  handleModalIsActive(
+                    project.photos,
+                    project.view_height,
+                    project.project_name
+                  )
+                }
+              />
+            </PhotoContainer>
+          ))}
+        </FlexRow2>
+      </FlexboxWrapper>
+      {/* <MobileFlexDisplay>
         {datatMobile.map((url, key) => (
-          <PhotoContainer key={key}>
+          <PhotoContainer key={key} className="mobile">
             <Photo
               src={`${url}`}
               onClick={() => handleModalIsActive(url, 'auto')}
             />
           </PhotoContainer>
         ))}
-      </MobileFlexDisplay>
+      </MobileFlexDisplay> */}
     </PortfolioMainWrapper>
   );
 };
