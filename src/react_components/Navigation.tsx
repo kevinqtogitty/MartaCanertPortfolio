5b074a6a50cac91497675c6da46fc35bc5fd89cd
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-10-06 18:58:21 +0200

    Added logo

--- src/react_components/Navigation.tsx
+++ src/react_components/Navigation.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
+import styled from 'styled-components';
 import { NavigationBar, NavigationLink } from '../styles/SC_navigation';
 
 const Navigation = () => {
   return (
     <NavigationBar>
-      <NavigationLink to="/about">About</NavigationLink>
-      <NavigationLink to="/portfolio">Portfolio</NavigationLink>
-      <NavigationLink to="/contact">Contact</NavigationLink>
+      <ResizedLogo src="/icons/logo.jpg" alt="Marta Canert Design" />
+      <div>
+        <NavigationLink to="/about">About</NavigationLink>
+        <NavigationLink to="/portfolio">Portfolio</NavigationLink>
+        <NavigationLink to="/contact">Contact</NavigationLink>
+      </div>
     </NavigationBar>
   );
 };
 
 export default Navigation;
+
+const ResizedLogo = styled.img`
+  height: 3rem;
+  @media (max-width: 500px) {
+    display: none;
+  }
+`;
