ea3a5d2f13f1b2e90350a2971a989142bc70d353
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-10-06 17:05:44 +0200

    Added a fade in animation

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,17 @@
+import { config, useSpring } from '@react-spring/web';
 import React from 'react';
 import { Content, LandingPageContent } from '../styles/SC_landing_page';
 
-const Home = () => {
+const Home: React.FC = (): JSX.Element => {
+  const animateHomePage = useSpring({
+    from: { opacity: 0, transform: 'translateY(150%)' },
+    to: { opacity: 1, transform: 'translateY(0%)' },
+    config: config.slow
+  });
+
   return (
     <LandingPageContent>
-      <Content>
+      <Content style={animateHomePage}>
         Hello,
         <br></br>
         how are you today?
